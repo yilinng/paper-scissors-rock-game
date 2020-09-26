353e4fd20058ad7e199daf35a60aefe967149487
svt <wei9912@gmail.com>
2020-09-26 16:00:25 +0800

    paper and scissors and rock game

---
+++ js/app.js
@@ -0,0 +1,369 @@
+
+const allItems = document.querySelector('.playOne');
+const triangle = document.querySelector('.triangle');
+
+const itemList = [{'result':'rock','color':'border-red-500'},{'result':'paper','color':'border-blue-500'},{'result':'scissors','color':'border-yellow-500'}];
+
+const randomItem = itemList[Math.floor(Math.random()*itemList.length)];
+
+Array.from(document.querySelectorAll('.cursor-pointer'));  
+      //keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+      window.addEventListener("click",chooseItem);
+
+function chooseItem(e){
+
+	let item = e.target.parentElement;
+
+	//console.log(item.childNodes[1].value);
+	const youPickValue = item.childNodes[1].value;
+	//show input value
+	//console.log(item.getAttribute('class'));
+	let itemClass = item.getAttribute('class');
+	//console.log(itemClass);
+	//let itemSvg = item.children[1];
+	//get item SVG
+	//console.log(item.children[0].value);
+	//input value 'rock','paper','scissors'
+	if (itemClass.includes('cursor-pointer') == true) {
+
+		printItem(item, youPickValue);
+		
+	}
+
+	
+}  
+
+function printItem(item, youPickValue){
+		
+			
+		//display by grid https://tailwindcss.com/docs/grid-template-rows
+		//display target
+		allItems.classList.add('grid', 'grid-rows-2','grid-flow-col' ,'gap-8');
+		item.classList.remove('h-32', 'w-32','z-10','mt-40' ,'ml-20','-mt-12' ,'ml-48','-mt-12' ,'-ml-4');
+		item.classList.add('z-40','relative','h-48', 'w-48', 'youPick','-mt-20','ml-10');
+		allItems.innerHTML = item.outerHTML;
+
+	
+
+
+
+	let itemTextr = document.createElement('span');
+		itemTextr.setAttribute('class','z-40 font-bold text-white text-2xl thehouse')	
+		itemTextr.innerText = 'THE HOUSE PICKED';
+
+	allItems.appendChild(itemTextr);
+
+	
+	let itemTextl = document.createElement('span');
+		itemTextl.setAttribute('class','z-40 font-bold text-white text-2xl ml-20 youPicked')	
+		itemTextl.innerText = 'YOU PICKED';
+
+	//choose div to add
+	
+	//https://www.w3schools.com/JSREF/met_node_insertbefore.asp
+	allItems.insertBefore(itemTextl, allItems.childNodes[0]);
+
+	let housePick = document.createElement('div');
+	   housePick.setAttribute('class','rounded-full h-48 w-48 flex items-center justify-center bg-gray-800 -mt-20 mr-16 housePick');
+	//https://www.w3schools.com/JSREF/met_node_insertbefore.asp
+
+
+	allItems.insertBefore(housePick, allItems.childNodes[3]);
+	
+	let housePicks = document.querySelector('.housePick');
+	//console.log(allItems.childNodes);
+
+	printHouse(housePicks, youPickValue);
+	
+} 
+
+function printHouse(housePicks, youPickValue){
+
+	
+	setTimeout( houseChoose(housePicks), 3000);
+
+	
+	setTimeout( ruleToOutcome(youPickValue, housePicks), 3000);
+
+	const playAgain =  document.querySelector('.playAgain');
+
+	playAgain.addEventListener('click', function(){
+		
+		//console.log('work');
+
+		displayGame();
+	});
+}    
+
+function houseChoose(housePicks){
+	
+
+	//console.log(randomItem.result);
+	
+	let houseChooses = document.createElement('img');
+		houseChooses.src = 'images/icon-'+ randomItem.result +'.svg';
+		//console.log(houseChooses);
+
+		housePicks.innerHTML = houseChooses.outerHTML;
+		housePicks.classList.remove('bg-gray-800');
+		housePicks.classList.add(randomItem.color,'bg-white','border-8','shadow-outline','z-40');			
+}
+
+function ruleToOutcome(youPickValue, housePicks){
+
+	//console.log(youPickValue);
+
+	if (youPickValue == 'paper') {
+		if (randomItem.result == 'rock') {
+			//console.log("you wins");
+			winTurn();
+		}else if(randomItem.result == 'scissors'){
+			//console.log("you lose");
+			loseTurn();
+		}else{
+			//console.log("try again");
+			tryAgain();
+		}
+	}else if(youPickValue == 'rock'){
+		if (randomItem.result == 'scissors') {
+			//console.log("you wins");
+			winTurn();
+
+		}else if(randomItem.result == 'paper'){
+			//console.log("you lose");
+			loseTurn();
+
+		}else{
+			//console.log("try again");
+			tryAgain();
+
+		}
+	}else{
+		if (randomItem.result == 'paper') {
+			//console.log("you wins");
+			winTurn();
+
+		}else if(randomItem.result == 'rock'){
+			//console.log("you lose");
+			loseTurn();
+
+		}else{
+			//console.log("try again");
+			tryAgain();
+
+		}
+	}
+
+	function winTurn(){
+
+		allItems.classList.remove('grid-rows-2','-ml-24');
+		allItems.classList.add('grid-cols-3','grid-rows-2');
+
+		//console.log(allItems.childNodes);
+
+		let centerText = document.createElement('div');
+		centerText.setAttribute('class','z-40 place-self-center font-bold text-white text-3xl centerPick')	
+		centerText.innerText = 'YOU Win!!';
+
+		//choose div to add
+	
+		//https://www.w3schools.com/JSREF/met_node_insertbefore.asp
+		allItems.insertBefore(centerText, allItems.childNodes[2]);
+
+		let centerPick = document.querySelector('.centerPick');
+		
+		let centerBtn = document.createElement('button');
+		centerBtn.setAttribute('class','z-40 place-self-center bg-white text-3xl text-pink-400 rounded p-2 -mt-20 playAgain');
+		centerBtn.innerText = 'Play again!';
+		
+		
+		allItems.insertBefore(centerBtn, allItems.childNodes[3]);
+
+			//console.log(allItems.childNodes);
+		let pointScore = document.querySelector('.point');
+		//console.log(typeof parseInt(pointScore.textContent) === 'number');
+		let pointNumber = parseInt(pointScore.textContent);
+		console.log(pointNumber);
+
+		pointNumber++;
+
+		pointScore.textContent = pointNumber;
+
+		let youPick = document.querySelector('.youPick');
+
+		let circle1 = document.createElement('div');
+		//https://tailwindcss.com/docs/background-opacity#app
+		//https://tailwindcss.com/docs/animation
+		circle1.setAttribute('class','z-30 animate-pulse absolute transform scale-100 rounded-full h-64 w-64  bg-opacity-25 shadow-2xl');
+		circle1.setAttribute('style','hsl(214, 47%, 23%)');
+
+		let circle2 = document.createElement('div');
+		circle2.setAttribute('class','z-20 animate-pulse absolute transform scale-125 rounded-full h-64 w-64  bg-opacity-50 shadow-2xl');
+		circle2.setAttribute('style','hsl(214, 47%, 23%)');
+
+		let circle3 = document.createElement('div');
+		circle3.setAttribute('class','z-10 animate-pulse absolute transform scale-150 rounded-full h-64 w-64  bg-opacity-100 shadow-2xl');
+		circle3.setAttribute('style','hsl(214, 47%, 23%)');
+
+		youPick.appendChild(circle1);
+
+		youPick.insertBefore(circle2,youPick.childNodes[0]);
+
+		youPick.insertBefore(circle3,youPick.childNodes[0]);
+
+
+	}
+
+	function loseTurn(){
+
+		allItems.classList.remove('grid-rows-2','-ml-24');
+		allItems.classList.add('grid-cols-3','grid-rows-2');
+
+		//console.log(allItems.childNodes);
+
+		let centerText = document.createElement('div');
+		centerText.setAttribute('class','z-40 place-self-center font-bold text-white text-3xl centerPick')	
+		centerText.innerText = 'YOU Lose!!';
+
+			//choose div to add
+	
+		//https://www.w3schools.com/JSREF/met_node_insertbefore.asp
+		allItems.insertBefore(centerText, allItems.childNodes[2]);
+
+		let centerPick = document.querySelector('.centerPick');
+		
+		let centerBtn = document.createElement('button');
+		centerBtn.setAttribute('class','z-40 place-self-center bg-white text-3xl text-pink-400 rounded p-2 -mt-20 playAgain');
+		centerBtn.innerText = 'Play again!';
+		
+		
+		allItems.insertBefore(centerBtn, allItems.childNodes[3]);
+
+			//console.log(allItems.childNodes);
+		let pointScore = document.querySelector('.point');
+		//console.log(typeof parseInt(pointScore.textContent) === 'number');
+		let pointNumber = parseInt(pointScore.textContent);
+		//console.log(pointNumber);
+
+		pointNumber--;
+
+		pointScore.textContent = pointNumber;
+
+
+		let circle1 = document.createElement('div');
+		//https://tailwindcss.com/docs/background-opacity#app
+		circle1.setAttribute('class','z-30 animate-pulse absolute rounded-full transform scale-100 h-64 w-64 m-auto bg-opacity-25 shadow-2xl circle1');
+		circle1.setAttribute('style','hsl(214, 47%, 23%)');
+		
+		let circle2 = document.createElement('div');
+		circle2.setAttribute('class','z-20 animate-pulse absolute rounded-full transform scale-125 h-64 w-64 border-white bg-opacity-50 shadow-2xl');
+		circle2.setAttribute('style','hsl(214, 47%, 23%) animation-delay: 1s');
+
+		let circle3 = document.createElement('div');
+		circle3.setAttribute('class','z-10 animate-pulse absolute transform scale-150 rounded-full h-64 w-64  bg-opacity-100 shadow-2xl');
+		circle3.setAttribute('style','hsl(214, 47%, 23%) animation-delay: 2s');
+
+
+		housePicks.appendChild(circle1);
+
+		housePicks.insertBefore(circle2,housePicks.childNodes[0]);
+
+		housePicks.insertBefore(circle3,housePicks.childNodes[0]);
+
+
+		
+	}
+
+	function tryAgain(){
+		allItems.classList.remove('grid-rows-2','-ml-24');
+		allItems.classList.add('grid-cols-3','grid-rows-2');
+
+		//console.log(allItems.childNodes);
+
+		let centerText = document.createElement('div');
+		centerText.setAttribute('class','place-self-center font-bold text-white text-3xl centerPick')	
+		centerText.innerText = 'draw Game!!';
+
+			//choose div to add
+	
+		//https://www.w3schools.com/JSREF/met_node_insertbefore.asp
+		allItems.insertBefore(centerText, allItems.childNodes[2]);
+
+		let centerPick = document.querySelector('.centerPick');
+		
+		let centerBtn = document.createElement('button');
+		centerBtn.setAttribute('class','place-self-center bg-white text-3xl text-pink-400 rounded p-2 -mt-20 playAgain');
+		centerBtn.innerText = 'Play again!';
+		
+		
+		allItems.insertBefore(centerBtn, allItems.childNodes[3]);
+
+			//console.log(allItems.childNodes);
+	}
+
+	
+
+}
+   function displayGame() {
+
+    const htmlString = `
+    		   
+
+    <div class="z-10 absolute -mt-12 -ml-4 rounded-full h-32 w-32 flex items-center justify-center bg-white border-8 border-blue-500 shadow-outline cursor-pointer paper">
+
+    <input type="hidden" value="paper">
+
+  		<img src="images/icon-paper.svg">
+
+</div>
+
+<div class="z-10 absolute  mt-40 ml-20 rounded-full h-32 w-32 flex items-center justify-center bg-white border-8 border-red-500 shadow-outline cursor-pointer rock">
+
+    <input type="hidden" value="rock">
+
+  		<img src="images/icon-rock.svg">
+
+</div>
+
+<div class="z-10 absolute -mt-12 ml-48 rounded-full h-32 w-32 flex items-center justify-center bg-white border-8 border-yellow-500 shadow-outline cursor-pointer scissors">
+
+      <input type="hidden" value="scissors">
+
+      <img src="images/icon-scissors.svg">
+
+
+</div>
+	<div class="relative triangle">
+		
+      <img src="images/bg-triangle.svg">
+
+	</div>
+
+
+      `;
+        
+        
+
+    allItems.innerHTML = htmlString;
+    allItems.classList.remove('grid','grid-cols-3','grid-rows-2','grid-flow-col' ,'gap-8');
+};
+
+//rule open and close
+const rulebtn = document.querySelector('.rulebtn');
+
+rulebtn.addEventListener('click',function(){
+
+const ruleForm = document.querySelector('.ruleForm');
+
+	ruleForm.classList.remove('hidden');
+
+	let cleanRule = document.querySelector('.cleanRule');
+
+	cleanRule.addEventListener('click', function(){
+
+		ruleForm.classList.add('hidden');
+		
+	});	
+
+
+});
